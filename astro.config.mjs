b21f847fb0d5d2d5b1cedb2f331491a3240f7b1e
Pruden <prudencosta@gmail.com>
2026-01-19 13:56:53 +0100

    Config astro para web github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 
 export default defineConfig({
+    site: 'https://prudenk.github.io',
+    base: '/madness-mad-web',
     vite: {
         plugins: [tailwindcss()]
     }
